c470b387c4852285da2e36b33f1483026c1b4524
AngeloEvangelista <angelodange@gmail.com>
2024-03-04 18:14:13 -0500

    3

---
+++ AspnetCoreMVC/Bulky/BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base  (options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 10, Name = "Action1", DisplayOrder = 1 },
+                new Category { CategoryId = 11, Name = "Action2", DisplayOrder = 2 },
+                new Category { CategoryId = 12, Name = "Action3", DisplayOrder = 3 }
+                );
+        }
+    }
+}
---
+++ AspnetCoreMVC/Bulky/BulkyWeb/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        
+        public int DisplayOrder { get; set; }
+    }
+}
